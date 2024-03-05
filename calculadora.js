db11ea161cd254418211635429b49799a6527b50
suzkx <126276347+suzkx@users.noreply.github.com>
2024-03-05 15:36:24 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+function insert(num){
+   var numero= document.getElementById("resultado").innerHTML;
+   document.getElementById("resultado").innerHTML= numero + num;
+}
+
+function back(){
+    var resultado=document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML= resultado.substring(0, resultado.length -1);
+}
+
+function calcular(){
+    var resultado=document.getElementById("resultado").innerHTML;
+    if(resultado){
+       document.getElementById("resultado").innerHTML=eval(resultado);
+    }
+};
